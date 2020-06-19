542c2ee38dd4ad85139e363430d1be001c12d969
Thanh <Thanh.Duong2503@gmx.de>
2020-06-19 11:39:24 +0200

    Add Header Grid

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Header.css";
+import Grid from '@material-ui/core/Grid';
 
 export default function Header() {
-    return <h1>Header Content</h1>
+    return(
+        <Grid   container direction="row"
+                justify="space-around"
+                alignItems="flex-start">
+
+            <Grid item xs={4}>
+                picture
+            </Grid>
+
+            <Grid item xs={8}>
+                Jokes
+            </Grid>
+
+        </Grid>
+
+
+        );
 }
