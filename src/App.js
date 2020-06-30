4b0478f81662116e7c2a94e12ece6a82c39bdf0e
Thanh <Thanh.Duong2503@gmx.de>
2020-06-30 10:36:01 +0200

    Add switch button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import './App.css';
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import Button from "@material-ui/core/Button";
+
+export const ThemeContext = createContext("dark");
+
 function App() {
+
+    const [theme, setTheme] = useState("dark")
+
   return (
     <div className="App">
 
+        <Button onClick={()=> setTheme(theme === "dark" ? "light" : "dark")}>switch theme</Button>
+        <ThemeContext.Provider value={theme}>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
+        </ThemeContext.Provider>
 
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./Header.css";
 import Title from "./Header/Title";
 import MenuIcon from "./Header/MenuIcon";
 import AppBar from "@material-ui/core/AppBar";
+import {ThemeContext} from "./App";
 
 export default function Header() {
 
+    const theme = useContext(ThemeContext)
+
     return(
-        <AppBar className={"Header"} position="static">
+        <AppBar className={"Header"} position="static" color={theme === "dark" ? "primary" : "secondary"}>
         <Title></Title>
         <MenuIcon></MenuIcon>
         </AppBar>
